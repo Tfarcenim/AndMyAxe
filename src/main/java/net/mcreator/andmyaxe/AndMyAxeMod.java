529d08c893e7c8db40667ef3f55eef4f67126fa5
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-07-24 20:02:40 -0400

    add inventory search feature

--- src/main/java/net/mcreator/andmyaxe/AndMyAxeMod.java
+++ src/main/java/net/mcreator/andmyaxe/AndMyAxeMod.java
@@ -13,7 +13,12 @@
  */
 package net.mcreator.andmyaxe;
 
+import net.mcreator.andmyaxe.init.AndMyAxeModKeyMappings;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.NonNullList;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ClickType;
+import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -26,45 +31,56 @@ import org.apache.logging.log4j.LogManager;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.IEventBus;
+import org.spongepowered.asm.mixin.Mixin;
 
 @Mod(AndMyAxeMod.MODID)
 public class AndMyAxeMod {
-	public static final Logger LOGGER = LogManager.getLogger(AndMyAxeMod.class);
-	public static final String MODID = "and_my_axe";
+    public static final Logger LOGGER = LogManager.getLogger(AndMyAxeMod.class);
+    public static final String MODID = "and_my_axe";
 
     public AndMyAxeMod() {
-		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-		MinecraftForge.EVENT_BUS.addListener(this::onBreakSpeed);
-		MinecraftForge.EVENT_BUS.addListener(this::clientTick);
-	}
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.addListener(this::onBreakSpeed);
+        MinecraftForge.EVENT_BUS.addListener(this::clientTick);
+    }
 
-	private void onBreakSpeed(PlayerEvent.BreakSpeed event) {
-		Player player = event.getPlayer();
-		BlockState state = event.getState();
+    private void onBreakSpeed(PlayerEvent.BreakSpeed event) {
+        if (!AndMyAxeModKeyMappings.ACTIVE) return;
+        Player player = event.getPlayer();
+        BlockState state = event.getState();
 
-		Level level = player.level;
-		if (level.isClientSide) {
-			if (event.getNewSpeed() <= 1) {
-				boolean found = false;
-				for (int i = 0;i < 9;i++) {
-					ItemStack potentialTool = player.getInventory().getItem(i);
-					if (potentialTool.getDestroySpeed(state) > 1) {
-						player.getInventory().selected = i;
-						found = true;
-						break;
-					}
-				}
-				if (!found) {
+        Level level = player.level;
+        if (level.isClientSide) {
+            if (event.getNewSpeed() <= 1) {
+                boolean found = false;
+                for (int i = 0; i < 9; i++) {
+                    ItemStack potentialTool = player.getInventory().getItem(i);
+                    if (potentialTool.getDestroySpeed(state) > 1) {
+                        player.getInventory().selected = i;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    NonNullList<ItemStack> items = player.getInventory().items;
+                    int change = -1;
+                    for (int i = 9; i < items.size(); i++) {
+                        ItemStack potentialTool = items.get(i);
+                        if (potentialTool.getDestroySpeed(state) > 1) {
+                            change = i;
+                            break;
+                        }
+                    }
+                    if (change > -1) {
+                        InventoryMenu menu = player.inventoryMenu;
+                        Minecraft.getInstance().gameMode.handleInventoryMouseClick(menu.containerId, change, player.getInventory().selected, ClickType.SWAP, player);
+                    }
+                }
+            }
+        }
+    }
 
-				}
-			}
-		}
-	}
+    private void clientTick(TickEvent.ClientTickEvent event) {
 
-	static boolean needsSwap;
-	static int swapTo;
-
-	private void clientTick(TickEvent.ClientTickEvent event) {
-
-	}
+    }
 }
--- src/main/java/net/mcreator/andmyaxe/init/AndMyAxeModKeyMappings.java
+++ src/main/java/net/mcreator/andmyaxe/init/AndMyAxeModKeyMappings.java
@@ -18,30 +18,28 @@ import net.minecraft.client.KeyMapping;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = {Dist.CLIENT})
 public class AndMyAxeModKeyMappings {
-	public static final KeyMapping SWAPPER_STOPPER = new KeyMapping("key.and_my_axe.swapper_stopper", GLFW.GLFW_KEY_LEFT_SHIFT,
-			"key.categories.gameplay");
+//	public static final KeyMapping SWAPPER_STOPPER = new KeyMapping("key.and_my_axe.swapper_stopper", GLFW.GLFW_KEY_LEFT_SHIFT,
+	//		"key.categories.gameplay");
 	public static final KeyMapping SWAPPER_TOGGLER = new KeyMapping("key.and_my_axe.swapper_toggler", GLFW.GLFW_KEY_H, "key.categories.gameplay");
-	private static long SWAPPER_STOPPER_LASTPRESS = 0;
 
 	@SubscribeEvent
 	public static void registerKeyBindings(FMLClientSetupEvent event) {
-		ClientRegistry.registerKeyBinding(SWAPPER_STOPPER);
+		//ClientRegistry.registerKeyBinding(SWAPPER_STOPPER);
 		ClientRegistry.registerKeyBinding(SWAPPER_TOGGLER);
 	}
+
+	public static boolean ACTIVE = true;
 
 	@Mod.EventBusSubscriber({Dist.CLIENT})
 	public static class KeyEventListener {
 		@SubscribeEvent
 		public static void onKeyInput(InputEvent.KeyInputEvent event) {
 			if (Minecraft.getInstance().screen == null) {
-				if (event.getKey() == SWAPPER_STOPPER.getKey().getValue()) {
+				if (event.getKey() == SWAPPER_TOGGLER.getKey().getValue()) {
 					if (event.getAction() == GLFW.GLFW_PRESS) {
-						SWAPPER_STOPPER_LASTPRESS = System.currentTimeMillis();
-					} else if (event.getAction() == GLFW.GLFW_RELEASE) {
-						int dt = (int) (System.currentTimeMillis() - SWAPPER_STOPPER_LASTPRESS);
+						ACTIVE = !ACTIVE;
 					}
 				}
-
 			}
 		}
 	}
